25ecc4a940192586d659cc3e7e2790fe2b859659
tuzmusic <tuzmusic@gmail.com>
2019-06-26 16:08:26 -0400

    display a map

--- App.js
+++ App.js
@@ -1,19 +1,23 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import MapScreen from "./src/screens/MapScreen";
+import MapView from "react-native-maps";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    // <View style={styles.container}>
+    // <Text>Home Screen</Text>
+    // <MapView style={{ flex: 1 }} showsUserLocation={true} />
+    <MapScreen />
+    // </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
---
+++ src/screens/MapScreen.js
@@ -0,0 +1,120 @@
+import React, { Component } from "react";
+import MapView from "react-native-maps";
+import { View, Button, Text, Platform, TextInput } from "react-native";
+
+const CurrentRegionMarker = ({ currentRegion }) => {
+  return currentRegion && currentRegion.showMarker ? (
+    <Marker coordinate={currentRegion} pinColor={"green"} />
+  ) : null;
+};
+
+class MapScreen extends Component {
+  static navigationOptions = {
+    title: "Deals Near You"
+  };
+
+  componentDidMount = () => {
+    // setTimeout(automate.bind(this), 2000);
+  };
+
+  state = { region: null };
+
+  // onMarkerPress = station => {
+  //   this.setState({
+  //     region: { ...this.props.currentRegion, ...station.location }
+  //   });
+  // };
+
+  // onCalloutPress = station => {
+  //   this.props.setCurrentStationID(station.id);
+  //   this.props.navigation.navigate("StationDetail", {
+  //     title: station.title
+  //   });
+  // };
+  // beforePressPrediction = async () => {
+  //   console.log("beforePressPrediction");
+  //   await this.setState({ region: null });
+  // };
+  render() {
+    return (
+      <View style={styles.container}>
+        {/* <LoadingIndicator
+          message={"Loading Stations..."}
+          isVisible={this.props.isLoading}
+        /> */}
+        <MapView style={{ flex: 1 }} showsUserLocation={true}>
+          {/* <StationMarkers
+            // provider={MapView.PROVIDER_GOOGLE}
+            // region={this.props.currentRegion}
+            stations={this.props.stations}
+            onCalloutPress={this.onCalloutPress.bind(this)}
+            onMarkerPress={() => {}}
+            // onMarkerPress={this.onMarkerPress.bind(this)}
+          /> 
+          <CurrentRegionMarker currentRegion={this.props.currentRegion} />
+        */}
+        </MapView>
+        {/* <Callout style={styles.searchCallout}>
+          <AutoFillMapSearch
+            style={styles.calloutSearch}
+            beforeOnPress={this.beforePressPrediction.bind(this)}
+          />
+        </Callout>
+        <Callout style={[styles.locationButtonCallout, { right: 100 }]}>
+          <Button
+            onPress={() =>
+              this.props.setCurrentRegion({
+                ...Locations.cupertino,
+                showMarker: true
+              })
+            }
+            title={"Cupertino"}
+            style={styles.locationButton}
+          />
+        </Callout>
+        <LocationButton
+          onPress={() => {
+            // this.setState({ region: null });
+            this.props.getLocationAsync();
+          }}
+        /> */}
+      </View>
+    );
+  }
+}
+
+export default MapScreen;
+
+const styles = {
+  searchCallout: {
+    backgroundColor: "white",
+    opacity: 0.9,
+    borderRadius: 10,
+    top: 20,
+    left: 15,
+    right: 15
+  },
+  calloutSearch: {
+    marginLeft: 10,
+    marginRight: 10,
+    height: 40
+  },
+  locationButtonCallout: {
+    borderRadius: 10,
+    opacity: 0.7,
+    backgroundColor: "lightgrey",
+    bottom: 0,
+    right: 0,
+    margin: 10
+  },
+  locationButton: {
+    backgroundColor: "grey",
+    borderRadius: 10,
+    opacity: 0.8
+  },
+  container: {
+    flex: 1,
+    flexDirection: "column",
+    justifyContent: "center"
+  }
+};
