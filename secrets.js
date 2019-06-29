1a0d2b37a13d6df243b2b9e35d07d41d73eb9419
tuzmusic <tuzmusic@gmail.com>
2019-06-29 19:30:22 -0400

    add woocommerce api info to secrets, which should be ignored but isn't

--- secrets.js
+++ secrets.js
@@ -1,1 +1,3 @@
 export const GoogleMapsApiKey = "AIzaSyAkN5I5TH6VMX8DR4S9gDlDZgjuub326rI";
+export const WCKey = "ck_a99fc6be38b0de91552632b3e1679f017cd59de8";
+export const WCSecret = "cs_0580ad91374c907b1dff75e059a5100db8d7f58d";
