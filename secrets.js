3065f727e346650633966b6069a0706f138eeb62
tuzmusic <tuzmusic@gmail.com>
2019-07-08 13:23:15 -0400

    stop caching secrets

--- secrets.js
+++
@@ -1,3 +0,0 @@
-export const GoogleMapsApiKey = "AIzaSyAkN5I5TH6VMX8DR4S9gDlDZgjuub326rI";
-export const WCKey = "ck_c5e6cc5df435f10507f8a67855e0d15beaf742c7";
-export const WCSecret = "cs_69062c18b057f45a741b9d4f36cf943cc061d089";
