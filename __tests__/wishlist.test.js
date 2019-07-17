173938fdfd926785204d8290f83c946b5bb679a3
tuzmusic <tuzmusic@gmail.com>
2019-07-17 15:44:33 -0400

    write action creators and getCurrentWishlist

--- __tests__/wishlist.test.js
+++ __tests__/wishlist.test.js
@@ -1,5 +1,4 @@
 // @flow
-import User from "../src/models/User";
 import dealsReducer, { initialState } from "../src/redux/reducers/dealsReducer";
 import type { DealState } from "../src/redux/reducers/dealsReducer";
 import axios from "axios";
@@ -7,6 +6,14 @@ import MockAdapter from "axios-mock-adapter";
 import { WishlistUrls } from "../src/constants/constants";
 import * as fs from "fs";
 import * as path from "path";
+import cheerio from "cheerio";
+import {
+  getCurrentWishlist,
+  addToWishlist,
+  removeFromWishlist
+} from "../src/redux/actions/wishlistActions";
+import mockDeals from "../__mocks__/mockDeals";
+import Deal from "../src/models/Deal";
 
 describe("Wishlist actions - Reducer", () => {
   describe("start actions", () => {
@@ -66,16 +73,49 @@ describe("Wishlist actions - Reducer", () => {
   });
 });
 
-fdescribe("Wishlist redux actions", () => {
-  const mock = new MockAdapter(axios);
+describe("Wishlist redux actions", () => {
+  let deal;
 
-  it("mocks from the HTML file", async () => {
+  beforeAll(() => {
+    // Set up mock response for getWishlist
+    const mock = new MockAdapter(axios);
     const thePath = path.resolve(__dirname, "../__mocks__/wishlist-2.html");
     const mockHtml = fs.readFileSync(thePath);
     mock.onGet(WishlistUrls.get).reply(200, mockHtml.toString(), {
       "content-type": "text/html"
     });
-    const res = await axios(WishlistUrls.get);
-    console.log(res.data);
+
+    deal = new Deal(mockDeals[0]);
+  });
+
+  describe("helper functions and action creators", () => {
+    describe("getCurrentWishlist helper function", () => {
+      it("loads the current user's wishlist and returns the ids", async () => {
+        expect(await getCurrentWishlist()).toEqual([2094, 2122, 2129]);
+      });
+    });
+
+    describe("addToWishlist", () => {
+      it("takes a deal argument and returns an action to add the deal to the wishlist", () => {
+        expect(addToWishlist(deal)).toEqual({
+          type: "ADD_TO_WISHLIST_START",
+          id: deal.id
+        });
+      });
+    });
+
+    describe("removeFromWishlist", () => {
+      it("takes a deal and the current wishlist and returns an action to remove the deal from the wishlist", () => {
+        const wishlist = [1, 2, deal.id, 4];
+        expect(removeFromWishlist(deal, wishlist)).toEqual({
+          type: "REMOVE_FROM_WISHLIST_START",
+          removal: { id: deal.id, index: 2 }
+        });
+      });
+    });
+  });
+
+  describe("addToWishlistSaga", () => {
+    // const startAction = {type: }
   });
 });
--- src/redux/actions/wishlistActions.js
+++ src/redux/actions/wishlistActions.js
@@ -5,10 +5,36 @@ import type { Saga } from "redux-saga";
 import { call, put, select, takeEvery, all } from "redux-saga/effects";
 import Deal from "../../models/Deal";
 import type { DealAction } from "../reducers/dealsReducer";
+import cheerio from "cheerio";
+
+export function addToWishlist(deal: Deal): DealAction {
+  return {
+    type: "ADD_TO_WISHLIST_START",
+    id: deal.id
+  };
+}
+
+export function removeFromWishlist(deal: Deal, wishlist: number[]): DealAction {
+  return {
+    type: "REMOVE_FROM_WISHLIST_START",
+    removal: { id: deal.id, index: wishlist.indexOf(deal.id) }
+  };
+}
 
 export function* addToWishlistSaga(): Saga<void> {}
 
 export function* removeFromWishlistSaga(): Saga<void> {}
+
+export async function getCurrentWishlist(): Promise<number[]> {
+  const res = await axios(WishlistUrls.get);
+  const $ = cheerio.load(res.data);
+  const tags = $(".button.yith-wcqv-button");
+  const idStrings: string[] = [];
+  tags.each((i, tag) => {
+    idStrings.push(tag.attribs["data-product_id"]);
+  });
+  return idStrings.map(n => Number(n));
+}
 
 export default function* wishlistSaga(): Saga<void> {
   yield all([
